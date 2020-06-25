87bbd0ace7ba661f4d8b5381e201b3166cb56a4a
harriet-the-spy <dpcairns@gmail.com>
2020-06-25 13:12:06 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// get dom elements
+// get add-input-1
+const addInput1 = document.getElementById('add-input-1');
+// get add-input-2
+const addInput2 = document.getElementById('add-input-2');
+// get add-button
+const boogerButton = document.getElementById('add-button');
+// get the add-result span
+const boogerSpan = document.getElementById('add-result');
+
+// initialize state 
+
+// set the event handler to the button
+boogerButton.addEventListener('click', () => {
+    // in the click event handler . . .
+    // make sure we have the inputs (console.log)
+    // make sure we have the span
+    // make sure we have the .values of the inputs
+    const value1 = addInput1.value;
+    const value2 = addInput2.value;
+
+    // add the two values together
+    const sum = Number(value1) + Number(value2);
+
+    // update the textContent of the span
+    boogerSpan.textContent = sum;
+});
